268779cd88c3f778b332408ff0f0e80369aca748
Dolu1990 <charles.papon.90@gmail.com>
2024-10-29 13:19:53 +0100

    Update

--- src/main/scala/demo/Miaou.scala
+++ src/main/scala/demo/Miaou.scala
@@ -1,7 +1,7 @@
 package demo
 
 import spinal.core._
-import vexiiriscv.soc.demo.{MicroSoc, MicroSocParam}
+import vexiiriscv.soc.micro.{MicroSoc, MicroSocParam}
 import vexiiriscv.{ParamSimple, VexiiRiscv}
 
 object Miaou extends App{
